b76defd225c69c2a78fac5756980e80716356b60
simon <simon@2fbabf53-ca1d-0410-a60d-c2e6af3f5ead>
2010-06-16 11:43:19 +0000

    Fixed polII_index.py to work with genes with the same name on different chromosomes, and genes with similar start positions

--- scripts/polII_index.py
+++ scripts/polII_index.py
@@ -20,7 +20,7 @@ parser.add_option("-d", "--downstream", dest="downstream",
 		metavar="FILE", default=DEFAULT_DOWN)
 #parser.add_option("-z", "--zeroes", dest="zeroes", help="Print zeroes", action="store_true", default=False)
 
-sys.stderr.write("NOT FINAL!! STILL UNDER DEVELOPMENT\n")
+sys.stderr.write("WARNING: this script is not exhaustively tested!\n")
 (options, args) = parser.parse_args()
 
 if not options.polfile or not options.genefile:
@@ -48,8 +48,14 @@ def genes_to_promoters_and_exons(file, up, down):
 	if type == "GFF":
 		strand_col, start_col, end_col = 6, 3, 4
 
+	line_num = 1
 	while line:
+		
 		vals = line[:-1].split("\t")
+		if len(vals) <= strand_col:
+			sys.stderr.write("ERROR: Strand column not present at line %s in file %s\n" % (line_num,file))
+			sys.exit(1)
+		
 		if type == "BED":
 			name = vals[3]
 		else:
@@ -58,7 +64,7 @@ def genes_to_promoters_and_exons(file, up, down):
 			except:
 				name = vals[8]
 
-		name = "%s_%s_%s" % (name, vals[0], vals[start_col])
+		name = "%s_|_%s_%s" % (name, vals[0], vals[start_col])
 		
 		chr[name] = vals[0]
 		
@@ -69,8 +75,9 @@ def genes_to_promoters_and_exons(file, up, down):
 			exon_data[name] = [(int(vals[start_col]), int(vals[end_col]))]
 
 		line = f.readline()
+		line_num += 1
 	f.close()
-	exons = []
+	genes = []
 	location = {}
 	promoter = []
 	
@@ -83,21 +90,8 @@ def genes_to_promoters_and_exons(file, up, down):
 			else: 
 				promoter.append([chr[gene], 0, pos[0][0] + down, gene])
 			
-			# Whole gene body
-
 			if (pos[-1][1] - pos[0][0]) - down > 0:
-				exons.append([chr[gene], pos[0][0] + down, pos[-1][1], gene])
-				
-			# EXONS ONLY
-			#
-			#if (pos[0][1] - pos[0][0]) - down > 0:
-			#	exons.append([chr[gene], pos[0][0] + down, pos[0][1], gene])
-			#for p in pos[1:]:
-			#	if p[1] > pos[0][0] + down:
-			#		if p[0] > pos[0][0] + down:
-			#			exons.append([chr[gene], p[0] , p[1], gene])
-			#		else:
-			#			exons.append([chr[gene], pos[0][0] + down , p[1], gene])
+				genes.append([chr[gene], pos[0][0] + down, pos[-1][1], gene])
 						
 		else:		
 			if pos[-1][1] - down > 0:
@@ -106,44 +100,28 @@ def genes_to_promoters_and_exons(file, up, down):
 				promoter.append([chr[gene], 0, pos[-1][1] + up, gene])
 			
 			if pos[-1][1] - pos[0][0] > down:
-				exons.append([chr[gene], pos[0][0], pos[-1][1] - down, gene])
+				genes.append([chr[gene], pos[0][0], pos[-1][1] - down, gene])
 
-			
-			#if (pos[-1][1] - pos[-1][0]) - down > 0:
-			#	exons.append([chr[gene], pos[-1][0], pos[-1][1] - down, gene])
-			#for p in pos[:-1]:
-			#	if p[0] < pos[-1][1] - down:
-			#		if p[1] < pos[-1][1] - down:
-			#			exons.append([chr[gene], p[0] , p[1], gene])
-			#		else: 
-			#			exons.append([chr[gene], p[0] , pos[-1][1] - down, gene])
-
-	return promoter, exons, location
+	return promoter, genes, location
 
 upstream = int(options.upstream)
 downstream = int(options.downstream)
-promoter, exons, location = genes_to_promoters_and_exons(options.genefile, upstream, downstream)
+promoter, genes, location = genes_to_promoters_and_exons(options.genefile, upstream, downstream)
 tmp = NamedTemporaryFile()
 tmpname = tmp.name
 f = open(tmpname, "w")
 seen = {}
 
-print len(exons)
-print exons[:10]
-sys.exit()
+genes = dict([(x[3], x[:3]) for x in genes])
+promoter = dict([(x[3], x[:3]) for x in promoter])
 
-for row in promoter:
-	p = "%s:%s-%s" % (row[0], row[1], row[2])
-	s = "%s\t%s\t%s\t%s\n" % (row[0], row[1], row[2], row[3])
+for name,(chr,start,end) in promoter.items():
+	p = "%s\t%s\t%s" % (chr,start,end)
 	if not seen.has_key(p):
-		f.write(s)
-		seen[p] = [row[3]]
-	else:	
-		seen.append(row[3])
+		f.write("%s\n" % p)
+		seen[p] = 1
 f.close()
 
-#for k,v in seen.items():
-#	print "PROM\t%s\t%s" % (k,v)
 prom_track = SimpleTrack(tmpname)
 pol_track = SimpleTrack(options.polfile)
 result = peak_stats.peak_stats(prom_track, pol_track, peak_stats.number_formatter)
@@ -151,25 +129,23 @@ result = peak_stats.peak_stats(prom_track, pol_track, peak_stats.number_formatte
 prom_result = {}
 for row in result:
 	chr,start,end,num = row.split("\t")
-	prom_result[seen["%s:%s-%s" % (chr, start,end)]] = int(num)
+	prom_result["%s\t%s\t%s" % (chr,start,end)] = float(num)
 
+prom_result = dict([(n, prom_result["%s\t%s\t%s" % (chr,start,end)]) for n,(chr,start,end) in promoter.items()])
+
+#print prom_result
 
 #GENES
 tmp = NamedTemporaryFile()
 tmpname = tmp.name
 f = open(tmpname, "w")
 seen = {}
-for row in exons:
-	p = "%s:%s-%s" % (row[0], row[1], row[2])
-	s = "%s\t%s\t%s\t%s\n" % (row[0], row[1], row[2], row[3])
+for name,(chr,start,end) in genes.items():
+	p = "%s\t%s\t%s" % (chr,start,end)
 	if not seen.has_key(p):
-		f.write(s)
-		seen[p] = [row[3]]
-	else:
-		seen.append(row[3])
+		f.write("%s\n" % p)
+		seen[p] = 1
 f.close()
-#for k,v in seen.items():
-#	print "EXON\t%s\t%s" % (k,v)
 
 exon_track = SimpleTrack(tmpname)
 pol_track = SimpleTrack(options.polfile)
@@ -179,32 +155,28 @@ gene_result = {}
 gene_length = {}
 for row in result:
 	chr, start, end, num = row.split("\t")
-	gene = seen["%s:%s-%s" % (chr, start,end)]
-	chr,start,end,num = row.split("\t")
-	if gene_result.has_key(gene):
-		gene_result[gene] += int(num)
-	else:
-		gene_result[gene] = int(num)
-	if gene_length.has_key(gene):
-		gene_length[gene] += int(end) - int(start)
-	else:
-		gene_length[gene] = int(end) - int(start)
+	loc = "%s\t%s\t%s" % (chr,start,end)
+	gene_result[loc] = float(num)
 
+gene_result = dict([(n, gene_result["%s\t%s\t%s" % (chr,start,end)]) for n,(chr,start,end) in genes.items()])
+gene_length = dict([(n, end - start) for n,(chr,start,end) in genes.items()])
+
+#print gene_result
 
 print "location\tgene_name\t# of tags / total_length\t# of tags in promoter\t# of tags gene body, norm to promoter\tlog2(prom/genebody)\tlength body/prom"
 for gene in prom_result.keys():
-	prom = float(prom_result[gene])
+	prom = prom_result[gene]
 	body = 0
 	body_norm = 0
 	ratio = "NA"
 	length = upstream
 	rel_length = "NA"
 	if gene_result.has_key(gene):
-		body = float(gene_result[gene])
+		body = gene_result[gene]
 		body_norm = body / float(gene_length[gene]) * (upstream + downstream)
-		#body_norm = body 
 		ratio = log((prom + 1)/ (body_norm + 1))/log(2)
 		length = gene_length[gene] + upstream	
 		rel_length = (gene_length[gene] - downstream) / (upstream + downstream)
-	print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (location[gene], gene, (prom + body) /  length , prom, body_norm, ratio, rel_length )
+	name = gene.split("_|_")[0]
+	print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (location[gene], name, (prom + body) /  length , prom, body_norm, ratio, rel_length )
 
